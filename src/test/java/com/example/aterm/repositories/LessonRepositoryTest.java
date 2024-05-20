91f5e1e82e54c2229404fe229a9e5b053454243c
ibelichev <belichev1102@gmail.com>
2024-05-20 22:11:26 +0300

    начало разработки календаря преподавателей

---
+++ src/test/java/com/example/aterm/repositories/LessonRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.example.aterm.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LessonRepositoryTest {
+
+    @Test
+    void findBySubscription() {
+    }
+
+    @Test
+    void findLessonsBySubscriptionId() {
+    }
+
+    @Test
+    void findLessonsByPrepod() {
+    }
+}
