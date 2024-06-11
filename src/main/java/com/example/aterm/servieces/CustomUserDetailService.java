5ad6494218f92983c84212cbfd1b8a9dffd2d007
ibelichev <belichev1102@gmail.com>
2024-06-12 01:32:30 +0300

    findByEmail теперь возвращает Optional

--- src/main/java/com/example/aterm/servieces/CustomUserDetailService.java
+++ src/main/java/com/example/aterm/servieces/CustomUserDetailService.java
@@ -14,6 +14,8 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email);
+        return userRepository.findByEmail(email).orElse(
+                null
+        );
     }
 }
